c5b7be572b7afaa5fac7a47e7e4aa98fcc948dc7
Gustavo Henrique Silva <47002007+gushsw@users.noreply.github.com>
2025-03-15 12:18:18 -0300

    Update pipeline_extract_bronze.py

--- apacheairflow/dags/pipeline_extract_bronze.py
+++ apacheairflow/dags/pipeline_extract_bronze.py
@@ -1,79 +1,47 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.operators.email import EmailOperator
-from airflow.sensors.s3_key_sensor import S3KeySensor
 from datetime import datetime, timedelta
 import requests
 import pandas as pd
 import boto3
 import io
-import os
 
-S3_BUCKET = "abinbev_bucket_bronze"
-S3_KEY = "bronze/breweries.parquet.gzip"
-AWS_REGION = "us-east-1"  
+S3_BUCKET_BRONZE = "abinbev_bucket_bronze"
+AWS_REGION = "us-east-1"
 
-def fetch_and_upload_data():
+def fetch_and_upload_bronze():
     API_URL = "https://api.openbrewerydb.org/breweries"
     response = requests.get(API_URL)
-
     if response.status_code == 200:
-        data = response.json()
-
-        df = pd.DataFrame(data)
+        df = pd.DataFrame(response.json())
         buffer = io.BytesIO()
         df.to_parquet(buffer, compression="gzip")
         buffer.seek(0)
-
-        s3_client = boto3.client("s3", region_name=AWS_REGION)
-        s3_client.upload_fileobj(buffer, S3_BUCKET, S3_KEY)
-        print(f"Arquivo salvo no S3: s3://{S3_BUCKET}/{S3_KEY}")
+        boto3.client("s3", region_name=AWS_REGION).upload_fileobj(
+            buffer, S3_BUCKET_BRONZE, "bronze/breweries.parquet.gzip"
+        )
     else:
         raise Exception(f"Erro na API: {response.status_code}")
 
-# Configurações padrão da DAG
 default_args = {
     'owner': 'gustavo',
-    'depends_on_past': False,
     'start_date': datetime(2024, 3, 14),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    'email_on_failure': True,
-    'email': ['seuemail@example.com'],  
 }
 
 dag = DAG(
-    'brewery_pipeline_s3',
+    'bronze_layer_brewery',
     default_args=default_args,
-    description='Pipeline para processar dados de cervejarias e salvar no S3',
+    description='Captura dados da API e salva na camada Bronze',
     schedule_interval='@daily',
     catchup=False,
 )
 
-fetch_task = PythonOperator(
-    task_id='fetch_and_upload_data',
-    python_callable=fetch_and_upload_data,
+fetch_bronze_task = PythonOperator(
+    task_id='fetch_and_upload_bronze',
+    python_callable=fetch_and_upload_bronze,
     dag=dag,
 )
 
-file_sensor_task = S3KeySensor(
-    task_id='check_s3_file',
-    bucket_key=S3_KEY,
-    bucket_name=S3_BUCKET,
-    aws_conn_id='aws_default',  
-    poke_interval=30,  
-    timeout=300,  
-    mode='poke',
-    dag=dag,
-)
-
-email_alert_task = EmailOperator(
-    task_id='send_email_on_failure',
-    to='seuemail@example.com',
-    subject=' Falha na Pipeline Brewery',
-    html_content='A pipeline Brewery falhou. Verifique os logs no Airflow.',
-    trigger_rule='one_failed', r
-    dag=dag,
-)
-
-fetch_task >> file_sensor_task >> email_alert_task
+fetch_bronze_task
